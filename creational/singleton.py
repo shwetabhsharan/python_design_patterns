356abc7eeb9da10c6c581f46921eb405b75d2313
Shwetabh Sharan <ssharan@marketo.com>
2016-12-22 17:12:39 +0530

    Added singleton example for design pattern

--- creational/singleton.py
+++ creational/singleton.py
@@ -1,42 +1,20 @@
-class Singleton:
-    """ A python singleton """
+"""
+A class of which only one instance can exist
+Example, Prime Minister of India or __main__ method in Python
+"""
 
-    class __impl:
-        """ Implementation of the singleton interface """
-
-        def spam(self):
-            """ Test method, return singleton id """
-            return id(self)
-
-    # storage for the instance reference
-    __instance = None
+class MySingleton(object):
+    INSTANCE = None
 
     def __init__(self):
-        """ Create singleton instance """
-        # Check whether we already have an instance
-        if Singleton.__instance is None:
-            # Create and remember instance
-            Singleton.__instance = Singleton.__impl()
+        if self.INSTANCE is not None:
+            raise ValueError("An instantiation already exists!")
 
-        # Store instance reference as the only member in the handle
-        self.__dict__['_Singleton__instance'] = Singleton.__instance
+    @classmethod
+    def get_instance(cls):
+        if cls.INSTANCE is None:
+            cls.INSTANCE = MySingleton()
+        return cls.INSTANCE
 
-    def __getattr__(self, attr):
-        """ Delegate access to implementation """
-        return getattr(self.__instance, attr)
-
-    def __setattr__(self, attr, value):
-        """ Delegate access to implementation """
-        return setattr(self.__instance, attr, value)
-
-
-# Test it
-s1 = Singleton()
-print id(s1), s1.spam()
-
-s2 = Singleton()
-print id(s2), s2.spam()
-
-# Sample output, the second (inner) id is constant:
-# 8172684 8176268
-# 8168588 8176268
+obj1 = MySingleton.get_instance()
+obj2 = MySingleton()
